98e66053388d42152349eca7dafa29168aa0c05e
Dhruvraj Solanki <dhruvrajsolanki@Dhruvrajs-MacBook-Air.local>
2025-02-22 22:47:34 +0530

    Sample fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
     // const response = await axios.post(`http://localhost:3000/ai/get-review`, { code })
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const apiUrl = new URL("/ai/get-review", backendUrl).href;
+    console.log(apiUrl)
     const response = await axios.post(apiUrl, { code })
     setReview(response.data)
   }
